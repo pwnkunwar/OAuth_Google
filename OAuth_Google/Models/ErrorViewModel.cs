b600532692336ee7adc81f0cf5dbd3d4842d7994
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-30 11:04:09 +0545

    Implementation of Google Oauth

---
+++ OAuth_Google/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OAuth_Google.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
